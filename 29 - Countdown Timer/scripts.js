0626a7a55e953e71cd1d3878c55c69274bb5a7c5
hummingengineer <hummingengineer@gmail.com>
2020-06-28 22:45:43 +0900

    Feat: add buttons

--- 29 - Countdown Timer/scripts.js
+++ 29 - Countdown Timer/scripts.js
@@ -1,10 +1,17 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
+// anything that has a data-time attribute 
+const buttons = document.querySelectorAll('[data-time]');
 
 function timer(seconds) {
+  // clear any existing timers
+  clearInterval(countdown);
+  
   const now = Date.now(); // now is in milliseconds
   const then = now + seconds * 1000;
-  console.log({now, then});
+  displayTimeLeft(seconds);
+  displayEndTime(then);
 
   // Every single second, we need to display the amount of time left
   countdown = setInterval(() => {
@@ -43,9 +50,14 @@ function displayTimeLeft(seconds) {
 function displayEndTime(timestamp) {
   const end = new Date(timestamp);
   const hour = end.getHours();
+  const adjustedHour = hour > 12 ? hour - 12 : hour;
+  const minutes = end.getMinutes();
+  endTime.textContent = `Be Back At ${adjustedHour}:${minutes < 10 ? '0' : ''}${minutes}`;
 }
 
 function startTimer() {
   const seconds = parseInt(this.dataset.time);
   timer(seconds);
 }
+
+buttons.forEach(button => button.addEventListener('click', startTimer));
